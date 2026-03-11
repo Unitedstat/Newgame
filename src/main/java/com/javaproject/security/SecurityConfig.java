28eb8e7949cfa42352440c3137cfb04c0fe65356
Unitedstat <kuday2657@gmail.com>
2026-03-11 16:39:28 +0530

    Update SecurityConfig.java

--- src/main/java/com/javaproject/security/SecurityConfig.java
+++ src/main/java/com/javaproject/security/SecurityConfig.java
@@ -4,67 +4,61 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     private LoggingAccessDeniedHandler accessDeniedHandler;
-
-    @Autowired
-    public void setAccessDeniedHandler(LoggingAccessDeniedHandler accessDeniedHandler) {
-        this.accessDeniedHandler = accessDeniedHandler;
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Autowired
-    @Lazy
-    private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     private DataSource dataSource;
 
     /**
-     * Creates a bean of type JdbcUserDetailsManager that will be used in
-     * HomeController
-     * 
-     * @return an instance configured to use our datasource
-     * @throws Exception
+     * Password encoder bean
      */
     @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsManager() throws Exception {
-        // provides crud operations for users
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
-
-        // Link up with our datasource
-        jdbcUserDetailsManager.setDataSource(dataSource);
-        return jdbcUserDetailsManager;
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    /**
+     * JdbcUserDetailsManager bean used in HomeController
+     */
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
+        manager.setDataSource(dataSource);
+        return manager;
+    }
+
+    /**
+     * Authorization configuration
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
-                .antMatchers("/user/**").hasAnyRole("USER", "MANAGER") // sets up authorization
+                .antMatchers("/user/**").hasAnyRole("USER", "MANAGER")
                 .antMatchers("/secured/**").hasAnyRole("USER", "MANAGER")
                 .antMatchers("/manager/**").hasRole("MANAGER")
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/", "/**").permitAll() // allows access to index in templates
-                .and() // allows us to chain
-                .formLogin().loginPage("/login")
+                .antMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
                 .defaultSuccessUrl("/secured")
                 .and()
-                .logout().invalidateHttpSession(true)
+                .logout()
+                .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .and()
                 .exceptionHandling()
@@ -74,17 +68,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
     }
 
+    /**
+     * Authentication configuration
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .withDefaultSchema()
-                .passwordEncoder(passwordEncoder)
+                .passwordEncoder(passwordEncoder())
                 .withUser("bugs")
-                .password(passwordEncoder.encode("bunny")).roles("USER")
+                .password(passwordEncoder().encode("bunny"))
+                .roles("USER")
                 .and()
                 .withUser("daffy")
-                .password(passwordEncoder.encode("duck")).roles("USER", "MANAGER");
+                .password(passwordEncoder().encode("duck"))
+                .roles("USER", "MANAGER");
     }
 }
